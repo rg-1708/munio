872645314c7969c98d2b5b381480ab255f09436d
Ramil Gojayev <gojayram@fit.cvut.cz>
2023-09-29 01:05:32 +0200

    Initial Theming setup, with shadcn

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/theming/buttons/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
