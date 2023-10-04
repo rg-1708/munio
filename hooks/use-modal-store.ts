3df16e56eeef8f1db722dc3d68495aed789f64e0
Ramil Gojayev <gojayram@fit.cvut.cz>
2023-10-05 00:09:08 +0200

    Added : channel creation, trough modal.

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,24 +1,29 @@
 import { Server } from "@prisma/client";
-import {create} from "zustand"
+import { create } from "zustand";
 
-export type ModalType = "create server" | "invite" | "edit server" | "members";
+export type ModalType =
+  | "create server"
+  | "invite"
+  | "edit server"
+  | "members"
+  | "create channel";
 
 interface ModalData {
-    server?: Server;
+  server?: Server;
 }
 
-interface ModalStore{
-    type: ModalType | null;
-    data: ModalData;
-    isOpen: boolean;
-    onOpen: (type : ModalType, data?: ModalData) => void;
-    onClose: () => void;
+interface ModalStore {
+  type: ModalType | null;
+  data: ModalData;
+  isOpen: boolean;
+  onOpen: (type: ModalType, data?: ModalData) => void;
+  onClose: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
-    type: null,
-    data:{},
-    isOpen:false,
-    onOpen: (type,data = {}) => set({isOpen: true, type, data}),
-    onClose: () => set({type:null, isOpen: false})
-}))
+  type: null,
+  data: {},
+  isOpen: false,
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  onClose: () => set({ type: null, isOpen: false }),
+}));
